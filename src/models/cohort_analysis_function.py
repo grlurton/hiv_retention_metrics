6e140e05af22a5c170849fcaf75ea3a685641d86
Grégoire Lurton <grlurton@uw.edu>
2017-04-19 22:34:51 -0700

    adding different cohort definition and anlaysis date plot

--- src/models/cohort_analysis_function.py
+++ src/models/cohort_analysis_function.py
@@ -19,30 +19,7 @@ def get_ltfu_rate(patients_status_df):
     ltfu_perc = sum(patients_status_df.status == 'LTFU') / len(patients_status_df)
     return ltfu_perc
 
-dat_2012 = data[data.first_visit_date < '2012-01-01']
 
-len(dat_2012)
-
-ltfu_rates = []
-for year in ['2012-01-01' , '2013-01-01' , '2014-01-01' , '2015-01-01' , '2016-01-01']:
-    print(year)
-    status_dat = dat_2012.groupby(['country' , 'facility' , 'patient_id']).apply(status_patient , reference_date = '2012-01-01' , analysis_date = year)
-    perc_ltfu = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date']).apply(get_ltfu_rate)
-    if len(ltfu_rates) > 0 :
-        ltfu_rates = ltfu_rates.append(perc_ltfu)
-    if len(ltfu_rates) == 0 :
-        ltfu_rates = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date']).apply(get_ltfu_rate)
-
-import matplotlib.pyplot as plt
-%matplotlib inline
-
-vl  = data.facility.value_counts()
-for fac in vl.index :
-    try :
-        plt.plot(list(ltfu_rates[: , fac]) , 'k' , alpha = 0.09)
-    except KeyError :
-        continue
-plt.show()
 
 ## QUESTION What is the validation of a LTFU patient ? Never comes back ? Comes back in less than a year ?
 ## TODO Patient Object. fonction associee donne, pour une date donnee, un statut avec l'information disponible a ce moment la + le statut reel.
--- src/models/data_cohort_description.py
+++ src/models/data_cohort_description.py
@@ -1,28 +1,60 @@
 import pandas as pd
+import cohort_analysis_function as cohort_analysis
+
 
 data = pd.read_csv('../../data/processed/complete_data.csv')
 data = data.groupby('patient_id').apply(get_first_visit_date)
 
-
 dat_2012 = data[data.first_visit_date < '2012-01-01']
 
 ltfu_rates = []
-for year in ['2012-01-01' , '2013-01-01' , '2014-01-01' , '2015-01-01' , '2016-01-01']:
-    print(year)
-    status_dat = dat_2012.groupby(['country' , 'facility' , 'patient_id']).apply(status_patient , reference_date = '2012-01-01' , analysis_date = year)
-    perc_ltfu = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date']).apply(get_ltfu_rate)
-    if len(ltfu_rates) > 0 :
-        ltfu_rates = ltfu_rates.append(perc_ltfu)
-    if len(ltfu_rates) == 0 :
-        ltfu_rates = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date']).apply(get_ltfu_rate)
+for grace_period in [30 , 60 , 90 ,  180]:
+    print(str(grace_period) + ' Days for definition')
+    for year in ['2012-01-01' , '2013-01-01' , '2014-01-01' , '2015-01-01' , '2016-01-01']:
+        print('Computing for ' + year)
+        status_dat = dat_2012.groupby(['country' , 'facility' , 'patient_id']).apply(status_patient , reference_date = '2012-01-01' , analysis_date = year ,  grace_period = grace_period)
+        status_dat['grace_period'] = grace_period
+        perc_ltfu = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date' , 'grace_period']).apply(get_ltfu_rate)
+        if len(ltfu_rates) > 0 :
+            ltfu_rates = ltfu_rates.append(perc_ltfu)
+        if len(ltfu_rates) == 0 :
+            ltfu_rates = status_dat.reset_index().groupby(['country' , 'facility' , 'analysis_date' , 'grace_period']).apply(get_ltfu_rate)
 
 import matplotlib.pyplot as plt
 %matplotlib inline
 
 vl  = data.facility.value_counts()
+plt.subplot(2, 2, 1)
 for fac in vl.index :
     try :
-        plt.plot(list(ltfu_rates[: , fac]) , 'k' , alpha = 0.09)
-    except KeyError :
+        plt.plot(pd.to_datetime(list(ltfu_rates[:,fac,:,30].index.levels[1])) ,list(ltfu_rates[: , fac , : , 30]) , 'k' , alpha = 0.12)
+    except (KeyError , ValueError):
         continue
+plt.subplot(2, 2, 2)
+for fac in vl.index :
+    try :
+        plt.plot(pd.to_datetime(list(ltfu_rates[:,fac,:,60].index.levels[1])) , list(ltfu_rates[: , fac , : , 60]) , 'b' , alpha = 0.12)
+    except (KeyError , ValueError):
+        continue
+plt.subplot(2, 2, 3)
+for fac in vl.index :
+    try :
+        plt.plot(pd.to_datetime(list(ltfu_rates[:,fac,:,90].index.levels[1])) ,list(ltfu_rates[: , fac , : , 90]) , 'r' , alpha = 0.12)
+    except (KeyError , ValueError):
+        continue
+plt.subplot(2, 2, 4)
+for fac in vl.index :
+    try :
+        plt.plot(pd.to_datetime(list(ltfu_rates[:,fac,:,180].index.levels[1])) , list(ltfu_rates[: , fac , : , 180]) , 'g' , alpha = 0.12)
+    except (KeyError , ValueError):
+        continue
+
 plt.show()
+
+plt.plot(ltfu_rates[:,fac,:,180].index.levels[1] ,list(ltfu_rates[: , fac , : , 30]) , 'k' , alpha = 0.12)
+
+plt.plot(pd.to_datetime(list(ltfu_rates[:,fac,:,60].index.levels[1])) ,
+list(ltfu_rates[: , fac , : , 60])z , 'b' , alpha = 0.12)
+
+pd.to_datetime(list(ltfu_rates[:,fac,:,60].index.levels[1]))
+list(ltfu_rates[: , fac , : , 60])
