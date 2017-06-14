a45cc063bea59aa44613ebc23f1ac68e93970a89
Grégoire Lurton <grlurton@uw.edu>
2017-06-14 16:02:44 -0700

    Lintering make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,26 +17,27 @@ kenya_3_patients_url = os.environ.get("kenya_3_patients_url")
 
 haiti_metadata_url = os.environ.get("haiti_metadata_url")
 
-### Loading Kenya
-visits_1 , patients_1 = get_kenya.read_access_data(kenya_1_url)
-visits_2 , patients_2 = get_kenya.read_csv_data(kenya_2_visits_url , kenya_2_patients_url)
-visits_3 , patients_3 = get_kenya.read_csv_data(kenya_3_visits_url , kenya_3_patients_url)
+# Loading Kenya
+visits_1, patients_1 = get_kenya.read_access_data(kenya_1_url)
+visits_2, patients_2 = get_kenya.read_csv_data(kenya_2_visits_url,
+                                               kenya_2_patients_url)
+visits_3, patients_3 = get_kenya.read_csv_data(kenya_3_visits_url,
+                                               kenya_3_patients_url)
 
-data_ken_1 = get_kenya.format_tables(visits_1 , patients_1 , 'ken_fac_1')
-data_ken_2 = get_kenya.format_tables(visits_2 , patients_2 , 'ken_fac_2')
-data_ken_3 = get_kenya.format_tables(visits_3 , patients_3 , 'ken_fac_3')
+data_ken_1 = get_kenya.format_tables(visits_1, patients_1, 'ken_fac_1')
+data_ken_2 = get_kenya.format_tables(visits_2, patients_2, 'ken_fac_2')
+data_ken_3 = get_kenya.format_tables(visits_3, patients_3, 'ken_fac_3')
 
-### Loading Haiti
+# Loading Haiti
 data_haiti = get_haiti.read_metadata(haiti_metadata_url)
 
-
-## Combining all the data
+# Combining all the data
 data_all = data_ken_1.append(data_ken_2).append(data_ken_3).append(data_haiti)
 data_complete = data_all.dropna()
 
-data_complete.to_csv('../../data/processed/complete_data.csv' , index = False)
+data_complete.to_csv('../../data/processed/complete_data.csv', index=False)
 
-### TODO Check completeness of data exported from access to csv for Kenya facilities 2 and 3
-### IDEA Only load visit data ?
-### TODO for Haiti, get dict of form types
-### TODO format dates so they are all outputed in the same way
+# TODO Check completeness of data exported from access to csv for Kenya 2 and 3
+# IDEA Only load visit data ?
+# TODO for Haiti, get dict of form types
+# TODO format dates so they are all outputed in the same way
