3f9f760aa0c645cdfd47ecab808a0499642ce44a
Grégoire Lurton <grlurton@uw.edu>
2017-10-07 17:58:48 -0700

    Adding discontinuation data from Haiti data

--- src/data/get_haiti.py
+++ src/data/get_haiti.py
@@ -2,12 +2,17 @@ import pandas as pd
 
 metadata_columns_to_keep = ['patientid' , 'sitecode' , 'form_id' , 'visitdate' , 'nxtVisitDate' , 'createdate']
 
-def read_metadata(haiti_metadata_url , metadata_columns_to_keep=metadata_columns_to_keep):
+def read_metadata(haiti_metadata_url, haiti_disc_url, metadata_columns_to_keep=metadata_columns_to_keep):
     metadata = pd.read_csv(haiti_metadata_url , sep = '|')
     metadata = metadata[metadata_columns_to_keep]
     metadata.createdate = pd.to_datetime(metadata.createdate , errors = 'coerce')
     metadata.visitdate = pd.to_datetime(metadata.visitdate , errors = 'coerce')
     metadata.nxtVisitDate = pd.to_datetime(metadata.nxtVisitDate , errors = 'coerce')
-    metadata.columns = ['patient_id' , 'facility' , 'form_type' , 'visit_date' , 'next_visit_date' , 'date_entered']
-    metadata['country'] = 'Haiti'
-    return metadata
+
+    # Adding in the discontinuation data
+    disc_data = pd.read_csv(haiti_disc_url, sep = '|')
+    data = metadata.merge(disc_data)
+
+    data.columns = ['patient_id' , 'facility' , 'form_type' , 'visit_date' , 'next_visit_date' , 'date_entered', 'discDate', 'discType', 'reasonDescEn']
+    data['country'] = 'Haiti'
+    return data
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,4 @@
 import os
-#from dotenv import load_dotenv, find_dotenv
 import src.data.get_kenya as get_kenya
 import src.data.get_haiti as get_haiti
 
@@ -31,13 +30,17 @@ if kenya :
     data_ken_2 = get_kenya.format_tables(visits_2, patients_2, 'ken_fac_2')
     data_ken_3 = get_kenya.format_tables(visits_3, patients_3, 'ken_fac_3')
 
-haiti_metadata_url = os.environ.get("haiti_data")
+haiti_data = os.environ.get("haiti_data")
 
-haiti_metadata_url
+haiti_data
 
-haiti_metadata_url = haiti_metadata_url  + '/encBplus.txt'
+haiti_metadata_url = haiti_data  + '/encBplus.txt'
+haiti_disc_url = haiti_data + '/discBplus.txt'
 # Loading Haiti
-data_haiti = get_haiti.read_metadata(haiti_metadata_url)
+data_haiti = get_haiti.read_metadata(haiti_metadata_url, haiti_disc_url)
+
+data_haiti.head()
+
 
 # Combining all the data
 if kenya == True :
@@ -49,7 +52,6 @@ data_complete = data_all.dropna()
 data_complete.to_csv('data/processed/complete_data.csv', index=False)
 
 # TODO Check completeness of data exported from access to csv for Kenya 2 and 3
-# TODO Check for computing environment at start. May be have it in initial script.
 # IDEA Only load visit data ?
 # TODO for Haiti, get dict of form types
 # TODO format dates so they are all outputed in the same way
